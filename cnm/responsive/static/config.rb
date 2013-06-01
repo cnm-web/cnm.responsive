ed78412c88b1955203a29fb5909f76b3cea48d18
Alex Sielicki <alex@techmilo.com>
2013-06-01 17:16:56 -0600

    adding proof of concept bootstrap nav

--- cnm/responsive/static/config.rb
+++ cnm/responsive/static/config.rb
@@ -7,9 +7,10 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "javascripts"
 
-# Alex requiring Foundation
+# Alex requiring Foundation, Bootstrap, etc.
 require "zurb-foundation"
 require 'font-icons'
+require 'bootstrap-sass'
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
