aff7052f868b0a63921e146d3b73bf55ba04df4a
Elliot Konetzni <ekonetzni@gmail.com>
2013-08-14 16:28:43 -0600

    adjusted js for career coach to be .submit based instead of .click

--- cnm/responsive/static/utilities.js
+++ cnm/responsive/static/utilities.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    $("#career-coach-search").click(function(){
-        var url = "https://cnm.emsicareercoach.com/#action=loadSearchResults&Search=";
+    $("#career-coach-search").submit(function(){
+        var url = "https://cnm.emsicareercoach.com/#action=loadSearchResults&SearchType=occupation&Search=";
         window.location = url + $("#career-coach-query").text();
+    	return false;
     });
 });
