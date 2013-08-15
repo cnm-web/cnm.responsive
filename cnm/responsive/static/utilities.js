b20aeba9489de5a820b3131a2674eb66105f352f
Elliot Konetzni <ekonetzni@gmail.com>
2013-08-15 16:20:12 -0600

    fixed the js so search behaves as expected

--- cnm/responsive/static/utilities.js
+++ cnm/responsive/static/utilities.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#career-coach-search").submit(function(){
         var url = "https://cnm.emsicareercoach.com/#action=loadSearchResults&SearchType=occupation&Search=";
-        window.location = url + $("#career-coach-query").text();
+        window.location = url + $("#career-coach-query").val();
     	return false;
     });
 });
