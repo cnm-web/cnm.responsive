5c32203b397bf9bf8bd37624c9ddaab9f17a1ecb
Elliot Konetzni <ekonetzni@gmail.com>
2013-10-10 08:44:17 -0600

    updated silly quiz with new copy.

--- cnm/responsive/static/utilities.js
+++ cnm/responsive/static/utilities.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 		result = calc(answers);
 		
 		$("#quiz").css("display", "none");
-		$("#results p.you-are").text(" the " + shapes[result-1].name + " balloon!");
+		$("#results p.you-are").text(shapes[result-1].name);
 		$("#results p.description").text(shapes[result-1].description);
 		$("#results img").attr("src", shapes[result-1].image)
 		$("#parent-fieldname-title").text("The Results Are In!")
@@ -51,28 +51,33 @@ $(document).ready(function () {
 });
 var shapes = [
 	{
-		'name' 			: 'Kissing Bees',
-		'description' 	: 'You love love! You know how to show affection for others, you pride yourself on quality friendships and relationships and you always seek to please others.',
+		'name' 			: "You're one of the Kissing Bees balloons!",
+		'description' 	: "<p>You love love! You know how to show affection for others and you pride yourself on quality friendships and relationships and you always seek to please others.</p> 
+						   <p>Want to know more about bees? And kissing? Check out CNM's biology and chemistry programs. <a href='/registration'>Register now.</a></p>",
 		'image' 		: 'http://www.balloonfiesta.com/uploads/companydirectory/id71/LillyLittleBee.jpg'
 	},
 	{
-		'name' 			: 'Darth Vader',
-		'description' 	: 'You may be an evil genius, but deep down people know you have a soft streak for those you care about. Keep on soaring, Vader!',
+		'name' 			: "You're the Darth Vader balloon!",
+		'description' 	: "<p>You may be an evil genius, but deep down people know you have a soft streak for those you care about. Keep on soaring, Vader!</p>
+<p>Are you the next George Lucas? Check out CNM Film program to get started on the path to a galaxy far, far away. <a href='/registration'>Register now.</a></p>",
 		'image' 		: 'http://www.balloonfiesta.com/uploads/companydirectory/id6/darth-photo.jpg'
 	},
 	{
-		'name' 			: 'Creamland Cow',
-		'description' 	: 'You are cheerful, likable, and don’t take yourself too seriously. People enjoy being around you and turn to you when they need a fun friend.',
+		'name' 			: "You're the Creamland Cow balloon!",
+		'description' 	: "<p>You are cheerful, likable, and don’t take yourself too seriously. People enjoy being around you and turn to you when they need a fun friend.</p>
+						   <p>If you love  animals, check out CNM's Veterinary Technology Program! <a href='/registration'>Register now.</a></p>",
 		'image' 		: 'http://www.balloonfiesta.com/uploads/companydirectory/id70/Airabellecow.jpg'
 	},
 	{
-		'name' 			: 'Scuba Diver',
-		'description' 	: 'You are practical, capable and a good planner. You aren’t afraid to take risks, but like to know what’s ahead.',
+		'name' 			: "You're the Scuba Diver balloon!",
+		'description' 	: "<p>You are practical, capable and a good planner. You aren’t afraid to take risks, but like to know what’s ahead.</p>
+							<p>Dive into the world of career and technical education at CNM. The college offers a wide range of career and technical programs including culinary, welding, healthcare, construction, computers and so much more. <a href='/registration'>Register now.</a></p>",
 		'image' 		: 'http://www.balloonfiesta.com/uploads/companydirectory/id147/Scuba1L.jpg'
 	},
 	{
-		'name' 			: 'Jack',
-		'description' 	: 'You are fun loving and enjoy making people laugh. People come to you for a chuckle in times of need.',
+		'name' 			: "You're the Jack balloon!",
+		'description' 	: "<p>You are fun loving and enjoy making people laugh. People come to you for a chuckle in times of need.</p>
+							<p>Like helping people feel better? CNM has a large variety of healthcare programs, where you can learn to turn a frown upside down! <a href='/registration'>Register now.</a></p>",
 		'image' 		: 'http://www.balloonfiesta.com/uploads/companydirectory/id12/jack-photo.jpg'
 	}
 ]
